38d853cefbdeb77953c8ba7e2b87e86a2bf004cd
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2020-08-23 18:31:54 -0400

    Created main menu and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   {
-    path:'home',component:HomeComponent
+    path:'',component:HomeComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,18 @@ import { FormsModule } from '@angular/forms';
 //import { MatButtonModule,MatCheckboxModule,MatIconModule,MatInputModule,MatMenuModule, MatCardModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
-
+import { MatMenuModule } from '@angular/material/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -28,7 +33,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FormsModule,
     MatTabsModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule
     ],
   providers: [],
   bootstrap: [AppComponent]
